845e2d907867638918ad91d8fa09f247e64dfd77
Anh Phan <anhphan.csf@gmail.com>
2019-11-29 23:58:56 +0700

    bắt đầu làm import excel

--- ExamReg.Apps/Services/MStudent/StudentService.cs
+++ ExamReg.Apps/Services/MStudent/StudentService.cs
@@ -1,7 +1,10 @@
 ﻿using ExamReg.Apps.Common;
 using ExamReg.Apps.Entities;
+using ExamReg.Apps.Repositories;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,39 +17,118 @@ namespace ExamReg.Apps.Services.MStudent
         Task<Student> Create(Student student);
         Task<Student> Update(Student student);
         Task<Student> Delete(Student student);
-        Task<Student> BulkInsert(byte[] file);
+        Task<List<Student>> ImportExcel(byte[] file);
     }
     public class StudentService : IStudentService
     {
-        
-        public Task<Student> BulkInsert(byte[] file)
+        private IUOW UOW;
+        public StudentService(IUOW UOW)
+        {
+            this.UOW = UOW;
+        }
+        public async Task<int> Count(StudentFilter filter)
         {
             throw new NotImplementedException();
         }
 
-        public Task<int> Count(StudentFilter filter)
+        public async Task<Student> Create(Student student)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Student> Create(Student student)
+        public async Task<Student> Delete(Student student)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Student> Delete(Student student)
+        public async Task<List<Student>> List(StudentFilter filter)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<Student>> List(StudentFilter filter)
+        public async Task<Student> Update(Student student)
         {
             throw new NotImplementedException();
         }
 
-        public Task<Student> Update(Student student)
+        public async Task<List<Student>> ImportExcel(byte[] file)
         {
-            throw new NotImplementedException();
+            // Chuyển hoá dữ liệu excel từ byte về Student BO
+            List<Student> excelTemplates = await LoadFromExcel(file);
+
+            // Lấy list students trong database
+            List<Student> students = await UOW.StudentRepository.List(new StudentFilter());
+
+            // Duyệt qua các student BO trong excel để gán dữ liệu excel vào dữ liệu database
+            foreach (Student template in excelTemplates)
+            {
+                // Kiểm tra dữ liệu đã tồn tại hay chưa
+                Student student = students.Where(s => s.StudentNumber == template.StudentNumber).FirstOrDefault();
+
+                // Nếu chưa tồn tại thì thêm mới, nếu đã tồn tại thì update
+                try
+                {
+                    if (student == null)
+                    {
+                        student = new Student
+                        {
+                            Id = new Guid(),
+                            StudentNumber = template.StudentNumber,
+                            GivenName = template.GivenName,
+                            LastName = template.LastName,
+                            Username = Convert.ToString(template.StudentNumber),
+                            Password = Convert.ToString(template.StudentNumber),
+                            Birthday = template.Birthday,
+                            Email = template.Email
+                        };
+
+                        students.Add(student);
+                    }
+                    else
+                    {
+                        student.GivenName = template.GivenName;
+                        student.LastName = template.LastName;
+                        student.Birthday = template.Birthday;
+                        student.Email = template.Email;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+
+            // Tiến hành giao dịch với database thông qua repo
+            using (UOW.Begin())
+            {
+                try
+                {
+                    var result = await UOW.StudentRepository.BulkInsert(students);
+                    await UOW.Commit();
+                    return excelTemplates;
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async Task<List<Student>> LoadFromExcel(byte[] file)
+        {
+            List<Student> excelTemplates = new List<Student>();
+            using (MemoryStream ms = new MemoryStream(file))
+            {
+                using (var package = new ExcelPackage(ms))
+                {
+                    var worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                    for (int i = worksheet.Dimension.Start.Row + 1; i <= worksheet.Dimension.End.Row; i++)
+                    {
+                        // ...
+                    }
+                }
+            }
+            return excelTemplates;
         }
     }
 }
