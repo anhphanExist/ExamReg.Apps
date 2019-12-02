60751e65b228add23a3984cc750048eac05dce06
Anh Phan <anhphan.csf@gmail.com>
2019-12-03 00:14:18 +0700

    bắt đầu thêm controller cho màn hình của Student

---
+++ ExamReg.Apps/Controllers/exam-register-result/ExamRegisterResultController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamReg.Apps.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamReg.Apps.Controllers.exam_register_result
+{
+    public class ExamRegisterResultRoute : Root
+    {
+        private const string Default = Base + "/exam-register-result";
+    }
+
+    [Authorize(Policy= "CanRegisterExam")]
+    public class ExamRegisterResultController : ApiController
+    {
+        public ExamRegisterResultController(ICurrentContext CurrentContext) : base(CurrentContext)
+        {
+
+        }
+    }
+}
---
+++ ExamReg.Apps/Controllers/exam-register/ExamRegisterController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamReg.Apps.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamReg.Apps.Controllers.exam_register
+{
+    public class ExamRegisterRoute : Root
+    {
+        private const string Default = Base + "/exam-register-result";
+    }
+
+    [Authorize(Policy = "CanRegisterExam")]
+    public class ExamRegisterController : ApiController
+    {
+        public ExamRegisterController(ICurrentContext CurrentContext) : base(CurrentContext)
+        {
+
+        }
+    }
+}
