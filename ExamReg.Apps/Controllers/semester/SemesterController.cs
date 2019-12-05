39469f051d257b6b9776c5b86e50e4e6e3ceb6a5
Anh Phan <anhphan.csf@gmail.com>
2019-12-05 18:23:00 +0700

    init semester controller and service

---
+++ ExamReg.Apps/Controllers/semester/SemesterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamReg.Apps.Common;
+using ExamReg.Apps.Services.MSemester;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamReg.Apps.Controllers.semester
+{
+    public class SemesterRoute : Root
+    {
+        private const string Default = Base + "/semester";
+        public const string List = Default + "/list";
+        public const string Create = Default + "/create";
+        public const string Update = Default + "/update";
+        public const string Delete = Default + "/delete";
+    }
+
+    [Authorize(Policy = "CanManage")]
+    public class SemesterController : ApiController
+    {
+        private ISemesterService SemesterService;
+        public SemesterController(ICurrentContext CurrentContext,
+            ISemesterService SemesterService
+            ) : base(CurrentContext)
+        {
+            this.SemesterService = SemesterService;
+        }
+    }
+}
---
+++ ExamReg.Apps/Services/MSemester/SemesterService.cs
@@ -0,0 +1,39 @@
+﻿using ExamReg.Apps.Common;
+using ExamReg.Apps.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamReg.Apps.Services.MSemester
+{
+    public interface ISemesterService : IServiceScoped
+    {
+        Task<List<Semester>> List(SemesterFilter filter);
+        Task<Semester> Create(Semester semester);
+        Task<Semester> Update(Semester semester);
+        Task<Semester> Delete(Semester semester);
+    }
+    public class SemesterService : ISemesterService
+    {
+        public async Task<Semester> Create(Semester semester)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Semester> Delete(Semester semester)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Semester>> List(SemesterFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Semester> Update(Semester semester)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
