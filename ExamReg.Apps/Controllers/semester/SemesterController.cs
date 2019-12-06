826b9a0a7c69f344a5641befee38549fd19688c2
Anh Phan <anhphan.csf@gmail.com>
2019-12-06 21:33:24 +0700

    SEMESTER CONTROLLER --> chờ test

--- ExamReg.Apps/Controllers/semester/SemesterController.cs
+++ ExamReg.Apps/Controllers/semester/SemesterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ExamReg.Apps.Common;
+using ExamReg.Apps.Entities;
 using ExamReg.Apps.Services.MSemester;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -15,7 +16,6 @@ namespace ExamReg.Apps.Controllers.semester
         private const string Default = Base + "/semester";
         public const string List = Default + "/list";
         public const string Create = Default + "/create";
-        public const string Update = Default + "/update";
         public const string Delete = Default + "/delete";
     }
 
@@ -28,6 +28,65 @@ namespace ExamReg.Apps.Controllers.semester
             ) : base(CurrentContext)
         {
             this.SemesterService = SemesterService;
+        }
+
+        [Route(SemesterRoute.List), HttpPost]
+        public async Task<List<SemesterDTO>> List()
+        {
+            List<Semester> res = await SemesterService.List(new SemesterFilter
+            {
+                OrderBy = SemesterOrder.Code,
+                OrderType = OrderType.ASC
+            });
+            return res.Select(r => new SemesterDTO
+            {
+                Id = r.Id,
+                Code = r.Code,
+                StartYear = r.StartYear,
+                EndYear = r.EndYear,
+                IsFirstHalf = r.IsFirstHalf,
+                Errors = r.Errors
+            }).ToList();
+        }
+
+        [Route(SemesterRoute.Create), HttpPost]
+        public async Task<SemesterDTO> Create([FromBody] SemesterDTO semesterRequestDTO)
+        {
+            Semester newSemester = new Semester
+            {
+                StartYear = semesterRequestDTO.StartYear,
+                EndYear = semesterRequestDTO.EndYear,
+                IsFirstHalf = semesterRequestDTO.IsFirstHalf
+            };
+            Semester res = await SemesterService.Create(newSemester);
+            return new SemesterDTO
+            {
+                Id = res.Id,
+                StartYear = res.StartYear,
+                EndYear = res.EndYear,
+                IsFirstHalf = res.IsFirstHalf,
+                Code = res.Code,
+                Errors = res.Errors
+            };
+        }
+
+        [Route(SemesterRoute.Delete), HttpPost]
+        public async Task<SemesterDTO> Delete([FromBody] SemesterDTO semesterRequestDTO)
+        {
+            Semester semester = new Semester
+            {
+                Id = semesterRequestDTO.Id
+            };
+            Semester res = await SemesterService.Delete(semester);
+            return new SemesterDTO
+            {
+                Id = res.Id,
+                StartYear = res.StartYear,
+                EndYear = res.EndYear,
+                IsFirstHalf = res.IsFirstHalf,
+                Code = res.Code,
+                Errors = res.Errors
+            };
         }
     }
 }
---
+++ ExamReg.Apps/Controllers/semester/SemesterDTO.cs
@@ -0,0 +1,13 @@
+﻿using ExamReg.Apps.Common;
+
+namespace ExamReg.Apps.Controllers.semester
+{
+    public class SemesterDTO : DataDTO
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public short StartYear { get; set; }
+        public short EndYear { get; set; }
+        public bool IsFirstHalf { get; set; }
+    }
+}
